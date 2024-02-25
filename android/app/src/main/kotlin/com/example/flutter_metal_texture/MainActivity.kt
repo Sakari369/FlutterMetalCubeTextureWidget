4105eed7b868bdb58d3baff5ce5ea52448650495
charlieforward9 <62311337+charlieforward9@users.noreply.github.com>
2024-02-25 09:49:18 -0500

    chore: clean repo for relevance

--- android/app/src/main/kotlin/com/example/flutter_metal_texture/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_metal_texture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/flutter_sokol_metal_texture/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_sokol_metal_texture
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
